7b03fe10c96ea5d675ecb6778a4627ce8de2f983
Mario <mario.gruszczynski@gmail.com>
2024-04-13 23:55:37 +0200

    main scraper function devided into workers and queue

--- hotOuttakes/scrapeHayb.ts
+++ hotOuttakes/scrapeHayb.ts
@@ -2,31 +2,35 @@ import extractCoffeeOrigin from "./helpers/extractCoffeeOrigin.js";
 import launchBrowserAndNewPage from "./helpers/launchBrowser.js";
 import { ProductData } from 'shared';
 
-//design scraper architecture
-
 
 
 interface LinkCollector {
     newProduct: (product: ProductData) => void;
     onError: (error: Error) => boolean;
 }
+  
 
 type NewProductHandler = (product: ProductData) => void;
+type PageProceesor = (page: Page) => Promise<ProductData>;
 
-const getHAYBProductData = async (onNewProduct:NewProductHandler = () => {}): Promise<ProductData[]> => {
+  
+
+const getHAYBProductData = async (): Promise<ProductData[]> => {
+
     const { browser, page } = await launchBrowserAndNewPage();
     await page.goto('https://haybcoffee.pl/en/product-category/coffee/');
     const productData: ProductData[] = [];
 
+   
     const shopName = await page.$eval('.wp-block-columns > .wp-block-column:nth-child(2) > h2', (el) => {
         if (el.textContent) return el.textContent.trim();
         else return '';
     });
-
+    
     const hrefs = await page.evaluate(() => {
         return Array.from(document.querySelectorAll('.woocommerce-loop-product__link')).map(a => (a as HTMLAnchorElement).href);
     });
-    console.log("urls", hrefs)
+    
 
     for (const url of hrefs) {
         try {
@@ -37,16 +41,15 @@ const getHAYBProductData = async (onNewProduct:NewProductHandler = () => {}): Pr
             const productOrigin = extractCoffeeOrigin(productName);
             const productImage = await page.$eval('.woocommerce-product-gallery__image > a', el => el.getAttribute('href')) ?? '';
             const productPrice = await page.$eval('.price-value', (el) => `${el.textContent?.trim()}€`);
-            const product = {
-                shopName,
-                productName,
-                productPrice,
-                productImage,
-                productLink: url,
-                productOrigin,
-            }
-            onNewProduct(product)
-            // productData.push(product);
+
+            productData.push({
+            shopName,
+            productName,
+            productPrice,
+            productImage,
+            productLink: url,
+            productOrigin,
+            });
         } catch (error) {
             console.error(`Error processing URL ${url}:`, error);
         }
@@ -56,7 +59,5 @@ const getHAYBProductData = async (onNewProduct:NewProductHandler = () => {}): Pr
     return productData;
 };
 
-
-getHAYBProductData()
-
 export default getHAYBProductData;
+   
